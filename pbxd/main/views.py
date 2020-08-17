15523e204bbc3490cf6afa18e1de593e6ee3909d
Benjamin Roy <benroy@uw.edu>
2020-08-17 15:40:01 -0700

    Include a route for a readiness check for Kubernetes.

--- pbxd/main/views.py
+++ pbxd/main/views.py
@@ -2,9 +2,17 @@ from . import main
 from ..app import pbx
 
 
-@main.route('/healthz')
-def index():
+@main.route('/ready')
+def readiness():
     """
-    Return the result of a full self diagnostic check.
+    Report that a worker is ready to handle a request.
+    """
+    return 'OK'
+
+
+@main.route('/healthz')
+def liveness():
+    """
+    Check if the application is able to perform its function.
     """
     return pbx.ossi_command('display time', fields={"0007ff00": ""}, debug=False)
